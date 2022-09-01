74beb68b57b23a28e8cd6a392d7fc6156de756d0
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-09-01 19:34:15 +0100

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className="flex items-center">
+            <img className='rounded-full h-20 w-20' src="https://imgs.search.brave.com/U_DraTp__x_pK4qOs2F7q6B3O_gehSb-KYqdsU-GSk0/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5U/bHlVNjB4dUxuV0FW/SV9jbzEySEVRSGFI/YSZwaWQ9QXBp" alt=""/>
+        
+          <div>
+            <h1>xy_anon Draw</h1>
+            <p>User: ....</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
